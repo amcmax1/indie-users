9c962fd8d0657c35cb129a596493ea18a3647d38
amcmax1 <95625051+amcmax1@users.noreply.github.com>
2022-08-14 23:38:28 +0300

    build: react router packages added with changes to App and root elements

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
-console.log("App loaded");
-const App = () => (
-  <h1 className="font-bold underline">Indie Users Management App</h1>
-);
+import { Link } from "react-router-dom";
+
+const App = () => {
+  return (
+    <div>
+      <h1 className="font-bold underline">Indie Users Management App</h1>
+      <Link to="/customers">Customers</Link>
+    </div>
+  );
+};
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -2,7 +2,19 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./styles.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Customers from "./routes/customers";
+import CustomerDetails from "./routes/customerDetails";
 
 const container = document.getElementById("root");
 const root = createRoot(container);
-root.render(<App />);
+root.render(
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />}>
+        <Route path="customers" element={<Customers />} />
+        <Route path=":customerId" element={<CustomerDetails />} />
+      </Route>
+    </Routes>
+  </BrowserRouter>
+);
